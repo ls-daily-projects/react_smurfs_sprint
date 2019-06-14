1b2d823253a7aee00083d95083a0cccca3272d55
Miikis <28997062+Miikis@users.noreply.github.com>
2019-06-14 08:55:22 -0700

    Implement POST /smurfs

--- village/src/App.js
+++ village/src/App.js
@@ -23,10 +23,20 @@ class App extends Component {
         }
     }
 
+    updateSmurfs = async () => {
+        try {
+            const { data } = await axios.get("/smurfs")
+            this.setState({ ...this.state, smurfs: [...data] })
+        } catch (error) {
+            console.log(error)
+            alert(`Smurfs, bro. Couldn't load any of the little blue guys :(`)
+        }
+    }
+
     render() {
         return (
             <div className="App">
-                <SmurfForm />
+                <SmurfForm formDidSubmit={this.updateSmurfs} />
                 <Smurfs smurfs={this.state.smurfs} />
             </div>
         )
